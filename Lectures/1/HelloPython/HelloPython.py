111874340cfa9e205fa0706ab35e91df82f88440
Airat <mingaleev.1993@mail.ru>
2022-11-21 20:01:03 +0300

    init commit

---
+++ Lectures/1/HelloPython/HelloPython.py
@@ -0,0 +1,70 @@
+# #print('Hello World')
+# a = 123
+# b = 1.23
+# c = None
+# # print(a)
+# # print(b)
+# # print(c)
+# # print(type(a))
+# # print(type(b))
+# # print(type(c))
+
+# s = 'Hello World'
+# # print(s)
+# print(a, b, s)
+# print(f'{a}-{b}-{c}')
+
+# f = True
+# e = False
+# print(f, e)
+# print(type(f))
+
+# list = [1, 2, 3, 'hello']
+# print(list)
+
+# print('a')
+# a = input()
+# print(a)
+
+# print('b')
+# b = int(input())
+# print('c')
+# c = int(input())
+# print(c, '+', b, '=', c+b)
+#      + - * / //деление целое % **степень
+
+# a = 2.4
+# b = 3.2
+# c = round(a * b, 1)
+# print(c)
+# a = 3
+# a += 5
+# #print(a)
+# b = 6
+# s = 8
+# c = a>b and b < s # or или
+# #print(c)
+# f = [1, 2, 6]
+# print(not b in f)
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print(a)
+# elif a==b:
+#     print('a==b')
+# else:
+#     print(b)
+
+# while условие:
+#     hgkj
+# else:
+#     hgkgkk
+
+# list = [1,3,10]
+# for i in list:
+#     print(i)
+# text = 'a'
+# text.count
+# help(text.count)
+
